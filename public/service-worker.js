8eb87df85947d05f2ad0f61516d7b82a95553546
jodymantap <muhammad.adnand.jody@gmail.com>
2024-06-07 20:00:10 +0700

    fix: restore old service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,39 +14,3 @@ self.addEventListener("install", function (event) {
     })
   );
 });
-
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(function (response) {
-        return (
-          response ||
-          fetch(event.request).then(function (response) {
-            return caches.open(CACHE_NAME).then(function (cache) {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          })
-        );
-      })
-      .catch(function () {
-        return caches.match("/");
-      })
-  );
-});
-
-self.addEventListener("activate", function (event) {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
