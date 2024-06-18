84f70173e6f8b46e3063937436b7848893f3ca31
jodymantap <muhammad.adnand.jody@gmail.com>
2024-06-18 23:34:28 +0700

    style: adjust welcome page

--- app/(pages)/welcome/page.tsx
+++ app/(pages)/welcome/page.tsx
@@ -8,14 +8,15 @@ import {
   CardBody,
   Image,
   Highlight,
+  Spacer,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Welcome() {
   return (
     <main className={styles.main}>
-      <Box className="hero" mb="12">
-        <Heading size="lg" mb="4">
+      <Box className="hero" mb="16">
+        <Heading fontSize="36px" mb="4">
           <Highlight
             query={["Finfriend"]}
             styles={{
@@ -29,17 +30,34 @@ export default function Welcome() {
             Meet Finfriend, your personal finance friend &#x1F44B;
           </Highlight>
         </Heading>
-        <Image src="mockup.png" borderRadius="10" alt="App Mockup" mb="4" />
+
+        <Text fontWeight="400" mb="4">
+          A simple yet powerful personal finance app. Let Finfriend helps you
+          manage your money efforlessly!
+        </Text>
         <Link href="/demo" passHref>
           <Button colorScheme="purple">Try Demo</Button>
+        </Link>
+        <Link style={{ marginLeft: "16px" }} href="/auth" passHref>
+          <Button colorScheme="purple" variant="outline">
+            Log In
+          </Button>
         </Link>
       </Box>
 
       <Box className="feature">
-        <Card>
+        <Heading size="md" mb="4">
+          Why be friends with us?
+        </Heading>
+        <Text mb="4" fontWeight="400">
+          Join our circle and unlock these awesome features. One-time payment
+          for lifetime friendship!
+        </Text>
+        <Image src="mockup.png" borderRadius="10" alt="App Mockup" />
+        <Card mt="4">
           <CardBody>
             <Heading size="sm">Easy Transaction Management</Heading>
-            <Text fontWeight="300" pt="2">
+            <Text fontWeight="400" pt="2">
               Quickly add your transactions and categorize them as cash,
               non-cash, withdrawal, or cash deposit. Finfriend helps you keep
               everything organized and accessible.
@@ -49,7 +67,7 @@ export default function Welcome() {
         <Card mt="4">
           <CardBody>
             <Heading size="sm">Balance Differentiation</Heading>
-            <Text fontWeight="300" pt="2">
+            <Text fontWeight="400" pt="2">
               Easily differentiate between your cash balance and bank account
               balance. Finfriend keeps track of both, giving you a comprehensive
               view of your financial status.
@@ -59,7 +77,7 @@ export default function Welcome() {
         <Card mt="4">
           <CardBody>
             <Heading size="sm">Real-Time Google Sheets Sync</Heading>
-            <Text fontWeight="300" pt="2">
+            <Text fontWeight="400" pt="2">
               Say goodbye to manual exports. Finfriend automatically syncs your
               financial data with Google Sheets in real-time, ensuring you
               always have the most current information. This integration allows
