4ea801d837f5188c6a0f63d3429e97aafe35cf0b
jodymantap <muhammad.adnand.jody@gmail.com>
2024-07-01 12:48:34 +0700

    fix: add innerRef to data form

--- app/_components/DataForm.tsx
+++ app/_components/DataForm.tsx
@@ -138,6 +138,7 @@ export default function DataForm() {
   return (
     <>
       <Formik
+        innerRef={formikRef}
         validationSchema={validationSchema}
         initialValues={{
           item: "",
